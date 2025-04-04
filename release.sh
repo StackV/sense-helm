67b51e519371432771f46b22de0b70aa12ed9ce3
R-Jimenez <alberto.est.jimenez@gmail.com>
2025-04-04 08:35:11 -0400

    amend

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Set a github `token` within the default config file at $HOME/.cr.yaml
-cr package
+cr package orchestrator
 cr upload --packages-with-index --push --skip-existing --config $HOME/.cr.yaml
