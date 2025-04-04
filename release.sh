7bcc8f29aaf795c818c3dc42e296e2fb8f4c0e6c
R-Jimenez <alberto.est.jimenez@gmail.com>
2025-04-04 08:44:54 -0400

    update main ignore

--- release.sh
+++ release.sh
@@ -3,3 +3,4 @@
 # Set a github `token` within the default config file at $HOME/.cr.yaml
 cr package orchestrator
 cr upload --packages-with-index --push --skip-existing --config $HOME/.cr.yaml
+cr index --packages-with-index --index-path . --push --config $HOME/.cr.yaml
