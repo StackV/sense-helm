d0c70d0b9fe1aadf82fe19a16fac0218b6afeaaf
R-Jimenez <alberto.est.jimenez@gmail.com>
2025-04-05 18:31:19 -0400

    add keycloak to release script.

--- release.sh
+++ release.sh
@@ -6,5 +6,6 @@
 # git-repo: sense-helm
 
 cr package orchestrator
+cr package keycloak
 cr upload --packages-with-index --push --skip-existing --config $HOME/.cr.yaml
 cr index --packages-with-index --index-path . --push --config $HOME/.cr.yaml
