e73463dcc09f7219c3bd61e95390e1fdfab306c6
Scyll Poesis <ourodaiko@gmail.com>
2025-07-17 07:39:58 -0400

    fix: Cleanup misleading script flags.

--- orchestrator/bin/create_secrets.sh
+++ orchestrator/bin/create_secrets.sh
@@ -3,17 +3,15 @@
 kubectl create secret generic sense-cred \
     --from-literal=tls-password='TLS_PASSWORD' \
     --from-literal=mysql-password='DB_PASSWORD' \
-    --from-literal=mail-password='MAIL_PASSWORD' \
-    -o yaml --dry-run=client
+    --from-literal=mail-password='MAIL_PASSWORD'
 
 #
 # Modify and run the following to create the required password secret for the chart:
 kubectl create secret generic sense-auth-cred \
     --from-literal=client='CLIENT' \
-    --from-literal=client-secret='CLIENT_SECRET' \
-    -o yaml --dry-run=client
+    --from-literal=client-secret='CLIENT_SECRET'
 
 #
 # Configure or download a valid JKS keystore and run the following to create the required keystore secret for the chart:
-kubectl delete secret sense-keystores --dry-run=client
-kubectl create secret generic sense-keystores --from-file=client.keystore -o yaml --dry-run=client
+# kubectl delete secret sense-keystores
+kubectl create secret generic sense-keystores --from-file=client.keystore
