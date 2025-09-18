0dfb03e67f3cdbe781b91b94a17b58a05e074932
R-Jimenez <alberto.est.jimenez@gmail.com>
2025-09-18 19:50:14 -0400

    feat: Finalize psql cluster definition.

--- keycloak/bin/create_secrets.sh
+++ keycloak/bin/create_secrets.sh
@@ -4,15 +4,4 @@ kubectl create secret generic RELEASE_NAME-auth --from-literal=admin-password='A
 
 #
 # Modify and run the following to create the required connection secret for separate PostgreSQL connection:
-kubectl create secret generic RELEASE_NAME-psql-auth --from-literal=host='DB_HOST' \
-    --from-literal=port='DB_PORT' \
-    --from-literal=username='DB_USERNAME' \
-    --from-literal=database='DB_DATABASE' \
-    --from-literal=password='DB_PASSWORD' 
-
-
-kubectl -n sense create secret generic new-keycloak-psql-auth --from-literal=host='new-keycloak-postgresql' \
-    --from-literal=port='5432' \
-    --from-literal=username='bn_keycloak' \
-    --from-literal=database='bitnami_keycloak' \
-    --from-literal=password='dbpassword' 
+kubectl create secret generic RELEASE_NAME-psql-auth --from-literal=username='keycloak' --from-literal=password='DB_PASSWORD' 
